f22e0d5d46bdd5a8cd0789a7c4efd1abb5d29ed1
Grobia <galaxia174@gmail.com>
2024-01-26 11:01:28 +0500

    first commit

---
+++ config.py
@@ -0,0 +1,20 @@
+
+class RostelecomInfo():
+    URL_START = 'https://start.rt.ru/'
+    URL_ELK = 'https://lk.rt.ru/'
+    URL_Key = 'https://key.rt.ru/'
+    URL_Online = 'https://my.rt.ru/'
+    URL_SmartHome = 'https://lk.smarthome.rt.ru/'
+
+    login = ""
+    invalid_login = ""
+    mail = ""
+
+
+    valid_password1 = ""
+    password2 = ""
+
+    tab_phone_text = "Номер"
+    tab_mail_text = "Почта"
+    tab_login_text = "Логин"
+    tab_personal_account_text = "Лицевой счёт"
